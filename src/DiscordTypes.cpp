8a6c977f69e13bb8b032d896b37fa61c29799bb4
pratik <pratikpd00@gmail.com>
2020-06-05 19:44:48 -0400

    added more methods

--- src/DiscordTypes.cpp
+++ src/DiscordTypes.cpp
@@ -2,6 +2,7 @@
 #include <Python.h>
 #include "discord_files/discord.h"
 
+using namespace discord;
 
 /*
  * Used for Discord activity objects in Python
@@ -14,21 +15,27 @@ typedef struct activityObject {
 
 
 PyObject *activity_getAppId(activityObject *self, void *closure) {
-    PyObject *id = Py_BuildValue("i", self->activty.GetApplicationID());
+    PyObject *id = Py_BuildValue("i", self->activity.GetApplicationID());
     Py_INCREF(id);
     return id;
 }
 
 PyObject *activity_getName(activityObject *self, void *closure) {
-    PyObject *name = Py_BuildValue("s", self->activty.GetName());
+    PyObject *name = Py_BuildValue("s", self->activity.GetName());
     Py_INCREF(name);
     return name;
 }
 
 PyObject *activity_getState(activityObject *self, void *closure) {
-    PyObject *state = Py_BuildValue("s", self->activty.GetState());
+    PyObject *state = Py_BuildValue("i", self->activity.GetState());
     Py_INCREF(state);
     return state;
+}
+
+PyObject *activity_getDetails(activityObject *self, void *closure) {
+    PyObject *details = Py_BuildValue("s", self->activity.GetDetails());
+    Py_INCREF(details);
+    return details;
 }
 
 int activity_setName(activityObject *self, PyObject *value, void *closure) {
@@ -44,4 +51,34 @@ int activity_setName(activityObject *self, PyObject *value, void *closure) {
     }
 
     self.activity.SetName(name);
+}
+
+int activity_setState(activityObject *self, PyObject *value, void *closure) {
+    if (value == NULL) {
+        PyErr_SetString(PyExc_TypeError, "cannot delete the name of an activity");
+        return -1;
+    }
+
+    char* state;
+    if (!PyArg_Parse(value, "s", &state)) {
+        PyErr_SetString(PyExc_TypeError, "can only set activity.state to an int");
+        return -1
+    }
+
+    self.activity.SetState(state);
+}
+
+int activity_setDetails(activityObject *self, PyObject *value, void *closure) {
+    if (value == NULL) {
+        PyErr_SetString(PyExc_TypeError, "cannot delete the name of an activity");
+        return -1;
+    }
+
+    char* details;
+    if (!PyArg_Parse(value, "s", &details)) {
+        PyErr_SetString(PyExc_TypeError, "can only set activity.state to an int");
+        return -1
+    }
+
+    self.activity.SetState(details);
 }
