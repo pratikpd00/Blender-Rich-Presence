0729d8eabab2b00b139f319fad37bb65c2242e47
pratik <pratikpd00@gmail.com>
2020-06-05 21:27:51 -0400

    finish activity object for python

--- src/DiscordTypes.cpp
+++ src/DiscordTypes.cpp
@@ -12,26 +12,15 @@ typedef struct {
     PyObject_HEAD;
     Activity activity;
     Core* core;
-} activityObject;
+} ActivityObject;
 
-
-static PyTypeObject activityType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "activity.Activity",
-    .tp_doc = "An object that manages rich presence for Blender",
-    .tp_basicsize = sizeof(activityObject),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
-};
-
-PyObject *activity_getDetails(activityObject *self, void *closure) {
+PyObject *activity_getDetails(ActivityObject *self, void *closure) {
     PyObject *details = Py_BuildValue("s", self->activity.GetDetails());
     Py_INCREF(details);
     return details;
 }
 
-int activity_setDetails(activityObject *self, PyObject *value, void *closure) {
+int activity_setDetails(ActivityObject *self, PyObject *value, void *closure) {
     if (value == NULL) {
         PyErr_SetString(PyExc_TypeError, "cannot delete the name of an activity");
         return -1;
@@ -46,7 +35,7 @@ int activity_setDetails(activityObject *self, PyObject *value, void *closure) {
     self->activity.SetState(details);
 }
 
-int activity_init(activityObject* self, PyObject* args, PyObject* kwds) {
+int activity_init(ActivityObject* self, PyObject* args, PyObject* kwds) {
     char* details = "";
     char* keywords[] = {"details", NULL};
 
@@ -61,6 +50,63 @@ int activity_init(activityObject* self, PyObject* args, PyObject* kwds) {
     self->activity.SetDetails(details);
     ActivityAssets& assets = self->activity.GetAssets();
     assets.SetLargeText("Blender");
-    assets.SetLargeImage("resources/blender_logo.png");
+    assets.SetLargeImage("blender_icon_1024x1024");
 
+    self->core->ActivityManager().UpdateActivity(self->activity);
+}
+
+PyObject* update(ActivityObject* self, PyObject* Py_UNUSED(ignored)) {
+    self->core->ActivityManager().UpdateActivity(self->activity);
+}
+
+
+static PyMethodDef ActivityMethod[] = {
+    {"update", (PyCFunction) update, METH_NOARGS, "Return the name, combining the first and last name"},
+    {NULL}
+};
+
+static PyGetSetDef getSetters[] = {
+    {"details", (getter) activity_getDetails, (setter) activity_setDetails, "The details about the Blender activity", NULL},
+    {NULL}
+};
+
+static PyTypeObject activityType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "activity.Activity",
+    .tp_doc = "An object that manages rich presence for Blender",
+    .tp_basicsize = sizeof(activityObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+    .tp_init = (initproc) activity_init,
+    .tp_methods = ActivityMethod,
+    .tp_getset = getSetters,
+};
+
+static PyModuleDef BlenderActivity = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "activity",
+    .m_doc = "Allows Blender's scripting engine to use Discord rich presence",
+    .m_size = -1,
+}
+
+PyMODINIT_FUNC PyInit_custom3() {
+    PyObject *module;
+    if (PyType_Ready(&activityType) < 0) {
+        return NULL;
+    }
+
+    module = PyModule_Create(&BlenderActivity);
+    if (m == NULL) {
+        return NULL;
+    }
+
+    Py_INCREF(&activityType);
+    if (PyModule_AddObject(m, "Custom", (PyObject *) &activityType) < 0) {
+        Py_DECREF(&activityType);
+        Py_DECREF(module);
+        return NULL;
+    }
+
+    return module;
 }
