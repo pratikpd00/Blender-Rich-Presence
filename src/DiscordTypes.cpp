fb1d286f4b9ceb717857486b98a3c81990e1bf97
pratik <pratikpd00@gmail.com>
2020-06-05 21:00:00 -0400

    start init

--- src/DiscordTypes.cpp
+++ src/DiscordTypes.cpp
@@ -1,5 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <time.h>
 #include "discord_files/discord.h"
 
 using namespace discord;
@@ -7,65 +8,27 @@ using namespace discord;
 /*
  * Used for Discord activity objects in Python
  */
-typedef struct activityObject {
-    PyObject_HEAD
+typedef struct {
+    PyObject_HEAD;
     Activity activity;
+    Core* core;
 } activityObject;
 
 
-
-PyObject *activity_getAppId(activityObject *self, void *closure) {
-    PyObject *id = Py_BuildValue("i", self->activity.GetApplicationID());
-    Py_INCREF(id);
-    return id;
-}
-
-PyObject *activity_getName(activityObject *self, void *closure) {
-    PyObject *name = Py_BuildValue("s", self->activity.GetName());
-    Py_INCREF(name);
-    return name;
-}
-
-PyObject *activity_getState(activityObject *self, void *closure) {
-    PyObject *state = Py_BuildValue("i", self->activity.GetState());
-    Py_INCREF(state);
-    return state;
-}
+static PyTypeObject activityType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "activity.Activity",
+    .tp_doc = "An object that manages rich presence for Blender",
+    .tp_basicsize = sizeof(activityObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+};
 
 PyObject *activity_getDetails(activityObject *self, void *closure) {
     PyObject *details = Py_BuildValue("s", self->activity.GetDetails());
     Py_INCREF(details);
     return details;
-}
-
-int activity_setName(activityObject *self, PyObject *value, void *closure) {
-    if (value == NULL) {
-        PyErr_SetString(PyExc_TypeError, "cannot delete the name of an activity");
-        return -1;
-    }
-
-    char* name;
-    if (!PyArg_Parse(value, "s", &name)) {
-        PyErr_SetString(PyExc_TypeError, "can only set activity.name to a string");
-        return -1
-    }
-
-    self.activity.SetName(name);
-}
-
-int activity_setState(activityObject *self, PyObject *value, void *closure) {
-    if (value == NULL) {
-        PyErr_SetString(PyExc_TypeError, "cannot delete the name of an activity");
-        return -1;
-    }
-
-    char* state;
-    if (!PyArg_Parse(value, "s", &state)) {
-        PyErr_SetString(PyExc_TypeError, "can only set activity.state to an int");
-        return -1
-    }
-
-    self.activity.SetState(state);
 }
 
 int activity_setDetails(activityObject *self, PyObject *value, void *closure) {
@@ -77,8 +40,27 @@ int activity_setDetails(activityObject *self, PyObject *value, void *closure) {
     char* details;
     if (!PyArg_Parse(value, "s", &details)) {
         PyErr_SetString(PyExc_TypeError, "can only set activity.state to an int");
-        return -1
+        return -1;
     }
 
-    self.activity.SetState(details);
+    self->activity.SetState(details);
+}
+
+int activity_init(activityObject* self, PyObject* args, PyObject* kwds) {
+    char* details = "";
+    char* keywords[] = {"details", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|s", keywords, &details)) {
+        return -1;
+    }
+
+    //client id is 0 to upload to public repo
+    Core::Create(0, DiscordCreateFlags_Default, &(self->core));
+
+    self->activity.SetName("Blender");
+    self->activity.SetDetails(details);
+    ActivityAssets& assets = self->activity.GetAssets();
+    assets.SetLargeText("Blender");
+    assets.SetLargeImage("resources/blender_logo.png");
+
 }
